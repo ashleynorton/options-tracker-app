50a04c03727d2de2c2cf67460d9618d9e5f2457f
ashleynorton <nortonmonet@gmail.com>
2020-06-30 22:16:18 -0400

    fill out get and post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,33 @@ class UsersController < ApplicationController
     erb :login
   end 
 
+  # the purpose of this route is to receive the login form,
+  #find the user, and log the user in (create a session)
+  post '/login' do 
+    
+    # Find the user
+    @user = User.find_by(email: params[:email])
+    # Authenticate the user - verify the user is who they say they are
+    # email/password combo
+    if @user.authenticate(params[:password])
+      # Log the user in - create the user session
+      session[:user_id] = @user.id # actually logging the user in
+      # Redirect to the user's show page
+      puts session
+      redirect "users/#{@user.id}"
+    else
+      # tell the user they entered invalid credentials
+      # redirect them to the login page
+    end 
+  end
+
   # what routes do I need for signup?
   get '/signup' do 
   
+  end
+
+  # user SHOW route
+  get '/users/:id' do 
+    "this will be the user show route"
   end
 end 
