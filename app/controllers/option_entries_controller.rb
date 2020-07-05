3d98f08f415edcaa7d34cec8cb3800cecdd352be
ashleynorton <nortonmonet@gmail.com>
2020-07-05 15:54:18 -0400

    add post entries

--- app/controllers/option_entries_controller.rb
+++ app/controllers/option_entries_controller.rb
@@ -7,7 +7,19 @@ class OptionEntriesController < ApplicationController
 
   # post option_entries to create a new option entry
   post '/option_entries' do 
-
+    # I want to create a new option entry and save it to the DB
+    # I only want to create an option entry if a user is logged in
+    if !logged_in?
+      redirect '/'
+    end
+    # I also only want to save the entry if it has some content
+    if params[:content] != ""
+      # create a new entry
+      @option_entry = OptionEntry.create(ticker: params[:ticker], option_type: params[:option_type], profit_or_loss: params[:profit_or_loss], user_id: current_user.id)
+      redirect "/option_entries/#{@option_entry.id}"
+    else 
+      redirect '/option_entries/new'
+    end 
   end 
 
   # show route for an option entry
