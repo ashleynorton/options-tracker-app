6f6cf45b583d93faa0feb95092d388b26c1f2926
ashleynorton <nortonmonet@gmail.com>
2020-07-05 15:15:42 -0400

    add option entries controller

---
+++ app/controllers/option_entries_controller.rb
@@ -0,0 +1,17 @@
+class OptionEntriesController < ApplicationController
+
+  # get options/new to render a form to creat new entry
+  get '/option_entries/new' do 
+    erb :'/option_entries/new' 
+  end 
+
+  # post option_entries to create a new option entry
+  post '/option_entries' do 
+
+  end 
+
+  # show route for an option entry
+
+
+  # index route for all option entries
+end 
